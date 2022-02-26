e9d4b9df227ca2ab8a8e140ef12607ce410134bb
Garett Pascual-Folster <31081158+GarettPF@users.noreply.github.com>
2022-02-26 14:29:25 -0800

    Add project files.

---
+++ HW3/main.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace HW3
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_image = new System.Windows.Forms.Button();
+            this.smoothed = new System.Windows.Forms.Button();
+            this.gradient = new System.Windows.Forms.Button();
+            this.edge_detection = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load_image
+            // 
+            this.load_image.Location = new System.Drawing.Point(12, 12);
+            this.load_image.Name = "load_image";
+            this.load_image.Size = new System.Drawing.Size(83, 23);
+            this.load_image.TabIndex = 0;
+            this.load_image.Text = "Load Image";
+            this.load_image.UseVisualStyleBackColor = true;
+            this.load_image.Click += new System.EventHandler(this.load_image_Click);
+            // 
+            // smoothed
+            // 
+            this.smoothed.Location = new System.Drawing.Point(12, 41);
+            this.smoothed.Name = "smoothed";
+            this.smoothed.Size = new System.Drawing.Size(83, 41);
+            this.smoothed.TabIndex = 1;
+            this.smoothed.Text = "Show Smoothed";
+            this.smoothed.UseVisualStyleBackColor = true;
+            this.smoothed.Click += new System.EventHandler(this.smoothed_Click);
+            // 
+            // gradient
+            // 
+            this.gradient.Location = new System.Drawing.Point(12, 88);
+            this.gradient.Name = "gradient";
+            this.gradient.Size = new System.Drawing.Size(83, 40);
+            this.gradient.TabIndex = 2;
+            this.gradient.Text = "Show Gradient";
+            this.gradient.UseVisualStyleBackColor = true;
+            this.gradient.Click += new System.EventHandler(this.gradient_Click);
+            // 
+            // edge_detection
+            // 
+            this.edge_detection.Location = new System.Drawing.Point(12, 134);
+            this.edge_detection.Name = "edge_detection";
+            this.edge_detection.Size = new System.Drawing.Size(83, 42);
+            this.edge_detection.TabIndex = 3;
+            this.edge_detection.Text = "Edge Detection";
+            this.edge_detection.UseVisualStyleBackColor = true;
+            this.edge_detection.Click += new System.EventHandler(this.edge_detection_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(101, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(648, 497);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 521);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.edge_detection);
+            this.Controls.Add(this.gradient);
+            this.Controls.Add(this.smoothed);
+            this.Controls.Add(this.load_image);
+            this.Name = "main";
+            this.Text = "CS 469 - Garett - Perform Canny Edge Detection on selected image";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button load_image;
+        private Button smoothed;
+        private Button gradient;
+        private Button edge_detection;
+        private PictureBox pictureBox;
+    }
+}
---
+++ HW3/main.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW3
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void load_image_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog imagefileopen = new OpenFileDialog();
+            imagefileopen.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png) |*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (imagefileopen.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(imagefileopen.FileName);
+                pictureBox.Size = pictureBox.Image.Size;
+            }
+        }
+
+        private Bitmap getGrayScale(Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            for (int j = 0; j < result.Height; j++)
+            {
+                for (int i = 0; i < result.Width; i++)
+                {
+                    Color color = image.GetPixel(i, j);
+                    int gray = (int)(color.R * 0.3 + color.G * 0.59 + color.B * 0.11);
+                    Color nColor = Color.FromArgb(gray, gray, gray);
+                    result.SetPixel(i, j, nColor);
+                }
+            }
+            return result;
+        }
+
+        private Bitmap getSmoothed(Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            int[,] filter = new int[,] {
+                {1,  4,  7,  4, 1},
+                {4, 16, 26, 16, 4},
+                {7, 26, 41, 26, 7},
+                {4, 16, 26, 16, 4},
+                {1,  4,  7,  4, 1},
+            };
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    int nPixel = 0;
+                    // apply filter to pixel
+                    for (int j = -2; j <= 2; j++)
+                    {
+                        for (int i = -2; i <= 2; i++)
+                        {
+                            int u = x + i;
+                            int v = y + j;
+                            if (u < 0 || u >= image.Width) u = x - i;
+                            if (v < 0 || v >= image.Height) v = y - j;
+
+                            Color value = image.GetPixel(u, v);
+                            nPixel += value.R * filter[2 + j, 2 + i];
+                        }
+                    }
+                    nPixel += 136;
+                    nPixel /= 273;
+                    Color color = Color.FromArgb(nPixel, nPixel, nPixel);
+                    result.SetPixel(x, y, color);
+                }
+            }
+
+            return result;
+        }
+
+        private struct Gradient
+        {
+            public double dx;
+            public double dy;
+            public double mag;
+            public int quadrant;
+        }
+
+        private Gradient[,] getGradient(Bitmap image)
+        {
+            Gradient[,] gradients = new Gradient[image.Width, image.Height];
+
+            for (int y = 1; y < image.Height-1; y++)
+            {
+                for (int x = 1; x < image.Width-1; x++)
+                {
+                    // calculate gradient vector
+                    int c1 = image.GetPixel(x+1, y).R;
+                    int c2 = image.GetPixel(x-1, y).R;
+                    double dx = (c1 - c2) / 2f;
+
+                    c1 = image.GetPixel(x, y + 1).R;
+                    c2 = image.GetPixel(x, y - 1).R;
+                    double dy = (c1 - c2) / -2f;
+
+                    // store dx and dy in array
+                    gradients[x, y].dx = dx;
+                    gradients[x, y].dy = dy;
+
+                    // store magnitude in array
+                    gradients[x, y].mag = Math.Abs(dx) + Math.Abs(dy);
+
+                    // calculate direction of gradient
+                    int q = 1;
+                    if (dx < 0 && dy > 0) q = 2;
+                    else if (dx < 0 && dy < 0) q = 3;
+                    else if (dx > 0 && dy < 0) q = 4;
+                    gradients[x, y].quadrant = q;
+                }
+            }
+
+            return gradients;
+        }
+
+        private Bitmap convertFromGradient(Bitmap image, Gradient[,] gradients)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+
+            for (int y = 1; y < image.Height-1; y++)
+            {
+                for (int x = 1; x < image.Width-1; x++)
+                {
+                    int c = (int)gradients[x, y].mag;
+                    Color color = Color.FromArgb(c, c, c);
+                    result.SetPixel(x, y, color);
+                }
+            }
+
+            return result;
+        }
+
+        private Bitmap nonMaxSuppression(Bitmap image, Gradient[,] gradients)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            int u = 0, v = 0;
+
+            for (int y = 1; y < image.Height - 1; y++)
+            {
+                for (int x = 1; x < image.Width - 1; x++)
+                {
+                    int value = image.GetPixel(x, y).R;
+                    Gradient pixel = gradients[x, y];
+                    if (pixel.quadrant == 1 || pixel.quadrant == 3) {
+                        if (Math.Abs(pixel.dx) > Math.Abs(pixel.dy))
+                        {
+                            u = (image.GetPixel(x + 1, y - 1).R + image.GetPixel(x + 1, y).R + 1) / 2;
+                            v = (image.GetPixel(x - 1, y + 1).R + image.GetPixel(x - 1, y).R + 1) / 2;
+                        }
+                        if (Math.Abs(pixel.dx) < Math.Abs(pixel.dy))
+                        {
+                            u = (image.GetPixel(x + 1, y - 1).R + image.GetPixel(x, y - 1).R + 1) / 2;
+                            v = (image.GetPixel(x - 1, y + 1).R + image.GetPixel(x, y + 1).R + 1) / 2;
+                        }
+                        if (Math.Abs(pixel.dx) == Math.Abs(pixel.dy))
+                        {
+                            u = image.GetPixel(x + 1, y - 1).R;
+                            v = image.GetPixel(x - 1, y + 1).R;
+                        }
+                    } else { 
+                        if (Math.Abs(pixel.dx) > Math.Abs(pixel.dy))
+                        {
+                            u = (image.GetPixel(x - 1, y - 1).R + image.GetPixel(x - 1, y).R + 1) / 2;
+                            v = (image.GetPixel(x + 1, y + 1).R + image.GetPixel(x + 1, y).R + 1) / 2;
+                        }
+                        if (Math.Abs(pixel.dx) < Math.Abs(pixel.dy))
+                        {
+                            u = (image.GetPixel(x - 1, y - 1).R + image.GetPixel(x, y - 1).R + 1) / 2;
+                            v = (image.GetPixel(x + 1, y + 1).R + image.GetPixel(x, y + 1).R + 1) / 2;
+                        }
+                        if (Math.Abs(pixel.dx) == Math.Abs(pixel.dy))
+                        {
+                            u = image.GetPixel(x - 1, y - 1).R;
+                            v = image.GetPixel(x + 1, y + 1).R;
+                        }
+                    }
+                    if (value > u && value > v)
+                    {
+                        result.SetPixel(x, y, image.GetPixel(x, y));
+                    } else
+                    {
+                        result.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private Bitmap hysteresis(Bitmap image, Gradient[,] gradients)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+
+            // get 5% threshold
+            int[] gList = new int[gradients.Length];
+            int write = 0;
+            for (int i = 1; i <= gradients.GetUpperBound(0) - 1; i++)
+            {
+                for (int j = 1; j <= gradients.GetUpperBound(1) - 1; j++)
+                {
+                    gList[write++] = (int)gradients[i, j].mag;
+                }
+            }
+
+            Array.Sort(gList);
+            int upperThreshold = gList[(int)(gList.Length * 0.95)];
+
+            // go through image and apply the threshold
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    if (gradients[i, j].mag > upperThreshold)
+                    {
+                        result.SetPixel(i, j, Color.White);
+                    } else
+                    {
+                        result.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void smoothed_Click(object sender, EventArgs e)
+        {
+            Form wn = new image();
+            wn.Text = "Smoothed Image";
+            Bitmap imageGrayScale = getGrayScale((Bitmap)pictureBox.Image);
+            Bitmap imageSmoothed = getSmoothed(imageGrayScale);
+
+            PictureBox picture = new PictureBox();
+            picture.Size = imageSmoothed.Size;
+            picture.Image = imageSmoothed;
+            wn.Controls.Add(picture);
+            wn.Show();
+        }
+
+        private void gradient_Click(object sender, EventArgs e)
+        {
+            Form wn = new image();
+            wn.Text = "Gradient Phase";
+            Bitmap imageGrayScale = getGrayScale((Bitmap)pictureBox.Image);
+            Bitmap imageSmoothed = getSmoothed(imageGrayScale);
+            Gradient[,] gradientArray = getGradient(imageSmoothed);
+            Bitmap imageGradient = convertFromGradient(imageSmoothed, gradientArray);
+
+            PictureBox picture = new PictureBox();
+            picture.Size = imageGradient.Size;
+            picture.Image = imageGradient;
+            wn.Controls.Add(picture);
+            wn.Show();
+        }
+
+        private void edge_detection_Click(object sender, EventArgs e)
+        {
+            Form wn = new image();
+            wn.Text = "Canny Edge Detection";
+            Bitmap imageGrayScale = getGrayScale((Bitmap)pictureBox.Image);
+            Bitmap imageSmoothed = getSmoothed(imageGrayScale);
+            Gradient[,] gradientArray = getGradient(imageSmoothed);
+            Bitmap imageGradient = convertFromGradient(imageSmoothed, gradientArray);
+            Bitmap imageEdgeDetect = nonMaxSuppression(imageGradient, gradientArray);
+            Bitmap finalImage = hysteresis(imageEdgeDetect, gradientArray);
+
+            PictureBox picture = new PictureBox();
+            picture.Size = finalImage.Size;
+            picture.Image = finalImage;
+            wn.Controls.Add(picture);
+            wn.Show();
+        }
+    }
+}
